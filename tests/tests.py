380a9841596549c1cbb9291773e0fb72d0c9c24f
Johannes Koester <johannes.koester@uni-due.de>
2023-12-07 14:50:45 +0100

    dbg

--- tests/tests.py
+++ tests/tests.py
@@ -38,4 +38,5 @@ class TestStorage(TestStorageBase):
     def test_storage_not_existing_dbg(self, tmp_path):
         obj = self._get_obj(tmp_path, self.get_query_not_existing(tmp_path))
         assert not obj.blob.exists()
+        print(obj.directory_entries(), file=sys.stderr)
         print(any(obj.directory_entries()), file=sys.stderr)
